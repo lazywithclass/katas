4430e29460fa4335be09921193b62ca4c24f65b7
lazywithclass <montecristoh@gmail.com>
2017-01-25 16:40:22 +0100

    removed useless describe

--- 3n+1/test.js
+++ 3n+1/test.js
@@ -4,21 +4,17 @@ describe('3n+1', function() {
       expect = require('chai').expect,
       fs = require('fs');
 
-  describe('3n+1', function() {
+  it('works', function() {
+    let input = fs.readFileSync('./input').toString().split('\n'),
+        output = fs.readFileSync('./output').toString().split('\n');
 
-    it('works', function() {
-      let input = fs.readFileSync('./input').toString().split('\n'),
-          output = fs.readFileSync('./output').toString().split('\n');
+    input.forEach((row, i) => {
+      let values = row.split(' ');
+      let result = lib(values[0], values[1]);
 
-      input.forEach((row, i) => {
-        let values = row.split(' ');
-        let result = lib(values[0], values[1]);
-
-        let expected = parseInt(output[i].split(' ')[2], 10);
-        expect(result).to.equal(expected);
-      });
+      let expected = parseInt(output[i].split(' ')[2], 10);
+      expect(result).to.equal(expected);
     });
-
   });
 
 });
